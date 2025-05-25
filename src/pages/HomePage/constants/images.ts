4b11afedbecaea25e57dd30e6ff6300cda9b48f3
타래 <85001878+meowTarae@users.noreply.github.com>
2025-05-25 22:10:17 +0900

    [🐛fix]: 이미지 경로별칭을 사용하지 않게끔 수정

--- src/pages/HomePage/constants/images.ts
+++ src/pages/HomePage/constants/images.ts
@@ -1,15 +1,8 @@
-const background1 = new URL(
-  '@/assets/HomePage/HomeBackground-1.webp',
-  import.meta.url
-).href;
-const image1 = new URL('@/assets/HomePage/HomeImage-1.webp', import.meta.url)
-  .href;
-const image2 = new URL('@/assets/HomePage/HomeImage-2.webp', import.meta.url)
-  .href;
-const image3 = new URL('@/assets/HomePage/HomeImage-3.webp', import.meta.url)
-  .href;
-const image4 = new URL('@/assets/HomePage/HomeImage-4.webp', import.meta.url)
-  .href;
+import background1 from '../../../assets/HomePage/HomeBackground-1.webp';
+import image1 from '../../../assets/HomePage/HomeImage-1.webp';
+import image2 from '../../../assets/HomePage/HomeImage-2.webp';
+import image3 from '../../../assets/HomePage/HomeImage-3.webp';
+import image4 from '../../../assets/HomePage/HomeImage-4.webp';
 
 export const HomePageImages = {
   background1,
