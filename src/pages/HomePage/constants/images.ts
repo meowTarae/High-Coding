cace7429afd07e7c4a775674a36ee3eb7b14568b
타래 <85001878+meowTarae@users.noreply.github.com>
2025-05-25 22:00:24 +0900

    [🐛fix]: 홈 이미지 import 경로 수정

--- src/pages/HomePage/constants/images.ts
+++ src/pages/HomePage/constants/images.ts
@@ -1,10 +1,13 @@
+import background1 from '@/assets/HomePage/HomeBackground-1.webp';
+import image1 from '@/assets/HomePage/HomeImage-1.webp';
+import image2 from '@/assets/HomePage/HomeImage-2.webp';
+import image3 from '@/assets/HomePage/HomeImage-3.webp';
+import image4 from '@/assets/HomePage/HomeImage-4.webp';
+
 export const HomePageImages = {
-  background1: new URL(
-    '@/assets/HomePage/HomeBackground-1.webp',
-    import.meta.url
-  ).href,
-  image1: new URL('@/assets/HomePage/HomeImage-1.webp', import.meta.url).href,
-  image2: new URL('@/assets/HomePage/HomeImage-2.webp', import.meta.url).href,
-  image3: new URL('@/assets/HomePage/HomeImage-3.webp', import.meta.url).href,
-  image4: new URL('@/assets/HomePage/HomeImage-4.webp', import.meta.url).href,
+  background1,
+  image1,
+  image2,
+  image3,
+  image4,
 };
